dc9d3516cf9f60113e2cdfea2a2bf81272c97fca
Dymedrol <dmitrykob@gmail.com>
2020-07-05 19:52:20 +0700

    Учебный проект: валидируй, как никогда не валидировал

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
+
 import {WelcomeScreen} from '../welcome-screen/welcome-screen.jsx';
 
 export const App = (props) => {
-  // eslint-disable-next-line react/prop-types
   const {gameTime, errorCount} = props;
 
   return <WelcomeScreen
     time={gameTime}
     errorCount={errorCount}
   />;
+};
+
+
+App.propTypes = {
+  gameTime: PropTypes.number.isRequired,
+  errorCount: PropTypes.number.isRequired,
 };
 
